01e77155c3853b69889802477c578ea12d3fd21f
nsddksmc <40565225+nsddksmc@users.noreply.github.com>
2020-05-01 22:54:53 +0300

    drcc4d2

---
+++ story_content/user.js
@@ -0,0 +1,31 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5pUF3YoOfnB":
+        Script1();
+        break;
+      case "5XLAJeD3wN2":
+        Script2();
+        break;
+      case "640yTjf0Xu6":
+        Script3();
+        break;
+  }
+}
+
+function Script1()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script2()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script3()
+{
+  $("#tab-customlink").hide();
+}
+
